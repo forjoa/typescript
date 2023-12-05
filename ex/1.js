d7c8ba0589cd1da7bd70c0e5ae2bc6c081707105
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-12-05 08:20:46 +0100

    Added new exercises

---
+++ ex/1.js
@@ -0,0 +1,5 @@
+var nam = 'joaquin';
+var greeting = function (name) {
+    console.log("Welcome ".concat(name));
+};
+greeting(nam);
---
+++ ex/1.ts
@@ -0,0 +1,7 @@
+const nam: string = 'joaquin'
+
+const greeting = (name: string) => {
+    console.log(`Welcome ${name}`)
+}
+
+greeting(nam)
